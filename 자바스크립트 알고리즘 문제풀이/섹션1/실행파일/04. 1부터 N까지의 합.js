9333493b3795a16f93705dc5249590089cc3137b
Yeonlisa <72447026+Yeonlisa@users.noreply.github.com>
2021-09-06 19:49:50 +0900

    Update 04. 1부터 N까지의 합.js

--- 자바스크립트 알고리즘 문제풀이/섹션1/실행파일/04. 1부터 N까지의 합.js
+++ 자바스크립트 알고리즘 문제풀이/섹션1/실행파일/04. 1부터 N까지의 합.js
@@ -1,10 +1,5 @@
 function solution(n){
-    let answer=0;
-    for(let i=1; i<=n; i++){
-        answer=answer+i;
-    }
-    
-    return answer;
+    return (n * (n + 1)) / 2;
 }
 
 console.log(solution(10));
