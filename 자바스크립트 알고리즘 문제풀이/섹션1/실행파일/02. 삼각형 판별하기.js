842ea99f98033f5ff588dddcdabc41bd5f874118
Yeonlisa <72447026+Yeonlisa@users.noreply.github.com>
2021-09-08 15:02:57 +0900

    Update 02. 삼각형 판별하기.js

--- 자바스크립트 알고리즘 문제풀이/섹션1/실행파일/02. 삼각형 판별하기.js
+++ 자바스크립트 알고리즘 문제풀이/섹션1/실행파일/02. 삼각형 판별하기.js
@@ -1,11 +1,10 @@
 function solution(a, b, c){
-    let answer="YES", max;
-    let tot=a+b+c;
-    if(a>b) max=a;
-    else max=b;
-    if(c>max) max=c;
-    if(tot-max<=max) answer="NO"; 
-    return answer;
+  let long = Math.max(a, b, c), total = a + b + c, oper = total - long;
+  if(oper > long) {
+    return "YES";
+  } else {
+    return "NO";
+  }
 }
 
 console.log(solution(13, 33, 17));
