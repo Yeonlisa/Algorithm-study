8ed81c58dce007309c525040a02f785143cf2978
Yeonlisa <72447026+Yeonlisa@users.noreply.github.com>
2021-09-11 20:01:06 +0900

    Update 01. 큰 수 출력하기.js

--- 자바스크립트 알고리즘 문제풀이/섹션2/실행파일/01. 큰 수 출력하기.js
+++ 자바스크립트 알고리즘 문제풀이/섹션2/실행파일/01. 큰 수 출력하기.js
@@ -1,10 +1,5 @@
 function solution(arr){         
-    let answer=[];
-    answer.push(arr[0]);
-    for(let i=1; i<arr.length; i++){
-        if(arr[i]>arr[i-1]) answer.push(arr[i]);
-    }
-    return answer;
+  return Math.max(...arr);
 }
 
 let arr=[7, 3, 9, 5, 6, 12];
